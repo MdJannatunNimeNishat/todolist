d47681cd42830dce11ccb2d9677fb6178f168b02
MdJannatunNimeNishat <baust.cse.160201077@gmail.com>
2021-04-28 00:17:03 +0600

    add item form done

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="todoListContainer">
+		<div class="heading">
+			<h1 id="title">Todo List</h1>
+			<add-item-form />
+			
+		</div>
+		<list-view/>
+	</div>
+</template>			
+
+<script>
+import addItemForm from "./addItemForm"
+import listView from "./listView"
+	 export default {
+		components: {
+			addItemForm,
+			listView
+		}
+	 }
+ </script> 
+
+<style scoped>
+ .todoListContainer {
+ 	width:350px;
+ 	margin: auto;
+ }	
+
+ .heading{
+ 	background: #e6e6e6;
+ 	padding:10px;
+ }
+
+#titel {
+	text-align: center;
+}
+
+
+</style>
